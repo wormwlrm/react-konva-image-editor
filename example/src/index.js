e176bc1c58f9755af98dc4db36766c847049288d
Jongyoon Jeong <wormwlrm@naver.com>
2021-10-08 00:11:13 +0900

    chore: 라이브러리 수정 시 examples에 바로 반영

---
+++ example/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Counter } from "konva-image-editor";
+
+const App = (
+  <div>
+    <Counter />
+  </div>
+);
+
+ReactDOM.render(App, document.getElementById("app"));
