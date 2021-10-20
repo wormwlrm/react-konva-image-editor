835c47a90cece932b8b981123bd374837a60f8b9
Jongyoon Jeong <wormwlrm@naver.com>
2021-10-20 20:09:48 +0900

    fix: Context 브릿지로 Stage에서도 Context 확인 가능하게 함

--- src/layout/Canvas.tsx
+++ src/layout/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { Stage, Layer } from 'react-konva';
 import Konva from 'konva';
@@ -9,9 +9,9 @@ import {
   TransformableRect
 } from '@/components';
 import {
-  useResizer, WindowSize
+  useResizer, useShapesContext, WindowSize
 } from '@/hooks';
-import { ShapesContext } from '@/context';
+import { ShapesConsumer, ShapesContext, ShapesProvider } from '@/context';
 
 const Canvas = ({
   width, height,
@@ -33,69 +33,80 @@ const Canvas = ({
     rectangles,
     texts,
     selected,
-  } = useContext(ShapesContext);
+    unfocus,
+    setFocused,
+  } = useShapesContext();
 
   const stage = useRef<Konva.Stage>();
 
   return (
-    <>
-      <Stage
-        ref={stage}
-        width={size.width}
-        height={size.height}
-        onMouseDown={unselect}
-        onTouchStart={unselect}
-      >
-        <Layer>
-          {circles.map((shape) => (
-            <TransformableCircle
-              key={shape.id}
-              {...shape}
-              radiusX={shape.radiusX}
-              radiusY={shape.radiusY}
-              isSelected={selected === shape.id}
-              onClick={() => setSelected(shape.id)}
-              onDragStart={(e) => onDragStart(e, shape)}
-              onDragEnd={(e) => onDragEnd(e)}
-              onTransform={(updated) => updateShape({
-                ...updated,
-                id: shape.id,
-              })}
-            />
-          ))}
-          {rectangles.map((shape) => (
-            <TransformableRect
-              key={shape.id}
-              {...shape}
-              isSelected={selected === shape.id}
-              onDragStart={(e) => onDragStart(e, shape)}
-              onDragEnd={(e) => onDragEnd(e)}
-              onClick={() => setSelected(shape.id)}
-              onTransform={(updated) => updateShape({
-                ...updated,
-                id: shape.id,
-              })}
-            />
-          ))}
-          {texts.map((shape) => (
-            <EditableText
-              key={shape.id!}
-              {...shape}
-              stage={stage.current}
-              isSelected={selected === shape.id}
-              onDragStart={(e) => onDragStart(e, shape)}
-              onDragEnd={(e) => onDragEnd(e)}
-              onClick={() => setSelected(shape.id)}
-              onTransform={(updated) => updateShape({
-                ...updated,
-                id: shape.id,
-              })}
-            />
-          ))}
-        </Layer>
-        <Layer name="top-layer" />
-      </Stage>
-    </>
+    <ShapesConsumer>
+      {(value) => (
+        // TODO: 키보드 이벤트 리스닝하게 하기
+        <Stage
+          ref={stage}
+          width={size.width}
+          height={size.height}
+          onMouseDown={(e) => {
+            unselect(e);
+            unfocus(e);
+          }}
+          onTouchStart={unselect}
+        >
+          <ShapesContext.Provider value={value}>
+            <Layer>
+              {circles.map((shape) => (
+                <TransformableCircle
+                  key={shape.id}
+                  {...shape}
+                  radiusX={shape.radiusX}
+                  radiusY={shape.radiusY}
+                  isSelected={selected === shape.id}
+                  onClick={() => setSelected(shape.id)}
+                  onDragStart={(e) => onDragStart(e, shape)}
+                  onDragEnd={(e) => onDragEnd(e)}
+                  onTransform={(updated) => updateShape({
+                    ...updated,
+                    id: shape.id,
+                  })}
+                />
+              ))}
+              {rectangles.map((shape) => (
+                <TransformableRect
+                  key={shape.id}
+                  {...shape}
+                  isSelected={selected === shape.id}
+                  onDragStart={(e) => onDragStart(e, shape)}
+                  onDragEnd={(e) => onDragEnd(e)}
+                  onClick={() => setSelected(shape.id)}
+                  onTransform={(updated) => updateShape({
+                    ...updated,
+                    id: shape.id,
+                  })}
+                />
+              ))}
+              {texts.map((shape) => (
+                <EditableText
+                  key={shape.id!}
+                  text={shape.text}
+                  {...shape}
+                  stage={stage.current}
+                  isSelected={selected === shape.id}
+                  onDragStart={(e) => onDragStart(e, shape)}
+                  onDragEnd={(e) => onDragEnd(e)}
+                  onClick={() => setSelected(shape.id)}
+                  onTransform={(updated) => updateShape({
+                    ...updated,
+                    id: shape.id,
+                  })}
+                />
+              ))}
+            </Layer>
+            <Layer name="top-layer" />
+          </ShapesContext.Provider>
+        </Stage>
+      )}
+    </ShapesConsumer>
   );
 };
 
